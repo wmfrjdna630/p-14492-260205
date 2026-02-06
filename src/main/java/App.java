f67a8be7d423efc63af4f30cebc992088a1c14f0
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2026-02-06 11:31:35 +0900

    1단계 ãGreen

---
+++ src/main/java/App.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class App {
+
+    private Scanner scanner;
+
+    public App(Scanner scanner){
+        this.scanner = scanner;
+    }
+
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+    }
+}
