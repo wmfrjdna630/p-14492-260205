be600070ca1d452ad06e71df6573c3f18bfd55d9
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2026-02-06 11:48:51 +0900

    level2 green

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,5 +10,8 @@ public class App {
 
     public void run() {
         System.out.println("== 명언 앱 ==");
+        System.out.println("명령) ");
+        System.out.println("명언 : ");
+        System.out.println("작가 : ");
     }
 }
