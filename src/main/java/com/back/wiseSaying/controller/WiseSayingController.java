d45293da285e9a6d2efa5a4aa74f8b8eeefcd622
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2026-02-06 14:42:30 +0900

    제대로함

--- src/main/java/com/back/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/back/wiseSaying/controller/WiseSayingController.java
@@ -1,19 +1,19 @@
 package com.back.wiseSaying.controller;
 
 import com.back.wiseSaying.entity.WiseSaying;
+import com.back.wiseSaying.service.WiseSayingService;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class WiseSayingController {
 
     private Scanner sc;
-    private List<WiseSaying> wiseSayings = new ArrayList<>();
-    private int lastId = 0;
+    private WiseSayingService wiseSayingService;
 
     public WiseSayingController(Scanner sc) {
         this.sc = sc;
+        this.wiseSayingService = new WiseSayingService();
     }
 
     public void actionAdd() {
@@ -22,19 +22,18 @@ public class WiseSayingController {
         System.out.print("작가 : ");
         String author = sc.nextLine();
 
-        lastId++;
-        WiseSaying wiseSaying = new WiseSaying(lastId, saying, author);
-        wiseSayings.add(wiseSaying);
+        WiseSaying wiseSaying = wiseSayingService.write(saying, author);
 
-        System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
+        System.out.println("%d번 명언이 등록되었습니다.".formatted(wiseSaying.getId()));
     }
 
     public void actionList() {
         System.out.println("번호 / 작가 / 명언");
         System.out.println("----------------------");
 
+        List<WiseSaying> wiseSayings = wiseSayingService.findListDesc();
+
         wiseSayings
-                .reversed()
                 .stream()
                 .forEach(wiseSaying -> System.out.printf("%d / %s / %s%n",
                         wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getSaying()));
