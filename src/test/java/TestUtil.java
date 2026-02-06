97fc701da1e02f798c350a5136ebc7eb7f612fb7
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2026-02-06 10:32:17 +0900

    TestUtil

---
+++ src/test/java/TestUtil.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TestUtil {
+
+//    public static String run(String input){
+//
+//        // 입력값
+//        Scanner scan = new Scanner(input);
+//
+//        // 명언 프로그램 실행
+//
+//
+//        // 출력값
+//
+//
+//    }
+
+    public static Scanner genScanner(String input) {
+        return new Scanner(input);
+    }
+}
---
+++ src/test/java/TestUtilTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestUtilTest {
+
+    @Test
+    @DisplayName("TestUtil.genScanner()")
+    void t1() {
+        Scanner scanner = TestUtil.genScanner("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
+
+        String cmd = scanner.nextLine();
+        String content = scanner.nextLine();
+        String author = scanner.nextLine();
+
+        assertThat(cmd).isEqualTo("등록");
+        assertThat(content).isEqualTo("현재를 사랑하라.");
+        assertThat(author).isEqualTo("작자미상");
+
+    }
+
+//    @Test
+//    @DisplayName("등록")
+//    void t1() {
+//        final String out = TestUtil.run("""
+//                등록
+//                현재를 사랑하라.
+//                작자미상
+//                """);
+//
+//        assertThat(out)
+//                .contains("명언 :")
+//                .contains("작가 :")
+//                .contains("1번 명언이 등록되었습니다.");
+//    }
+
+}
