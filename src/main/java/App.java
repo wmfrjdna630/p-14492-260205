e855c483a80e5ca9d591dd81fb2bd15e0b912f20
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2026-02-06 12:25:28 +0900

    level4 green

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,18 +10,23 @@ public class App {
 
     public void run() {
         System.out.println("== 명언 앱 ==");
-        System.out.println("명령) ");
-        String cmd = sc.nextLine();
-        switch (cmd) {
-            case "등록" -> {
-                System.out.print("명언 : ");
-                String saying = sc.nextLine();
-                System.out.print("작가 : ");
-                String author = sc.nextLine();
-                System.out.println("1번 명언이 등록되었습니다.");
-            }
-            case "종료" -> {
-                return;
+        int lastId = 0;
+
+        while(true) {
+            System.out.println("명령) ");
+            String cmd = sc.nextLine();
+            switch (cmd) {
+                case "등록" -> {
+                    System.out.print("명언 : ");
+                    String saying = sc.nextLine();
+                    System.out.print("작가 : ");
+                    String author = sc.nextLine();
+                    lastId++;
+                    System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
+                }
+                case "종료" -> {
+                    return;
+                }
             }
         }
     }
