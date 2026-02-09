252253929a8b9f0741eb52983cce733d6a3b07ef
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2026-02-09 12:50:41 +0900

    level 14 red 3 green 3

--- src/main/java/com/back/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/back/wiseSaying/controller/WiseSayingController.java
@@ -7,6 +7,7 @@ import com.back.wiseSaying.global.Rq;
 import com.back.wiseSaying.service.WiseSayingService;
 
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class WiseSayingController {
 
@@ -52,6 +53,19 @@ public class WiseSayingController {
                 .stream()
                 .forEach(wiseSaying -> System.out.printf("%d / %s / %s%n",
                         wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getSaying()));
+
+        System.out.print("페이지 : ");
+        IntStream
+                .rangeClosed(1, 2)
+                        .forEach((num) -> {
+                            if(num == page){
+                                System.out.print("[" + num + "]");
+                            }
+                            else {
+                                System.out.print(num);
+                            }
+                            System.out.print(" / ");
+                        });
 
     }
     public void actionDelete(Rq rq) {
